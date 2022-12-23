4b4f49b42c7fab460f9ca46647b58738ee34701a
kavindu9611 <kavinduwickramasinghe1920@gmail.com>
2022-12-24 03:14:58 +0530

    second commit

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 This is bitfumes node js tutorial
+
+Important git commands
+git add .
+git commit -m "first commit"
+git push https://github.com/kavindu9611/smilga-node-tutorial.git
--- app.js
+++ app.js
@@ -1,11 +1,46 @@
 const http = require('http');
+const fs = require('fs');
+const { throws } = require('assert');
 
-// Create a local server to receive data from
+const PORT = 3000;
+
+ //Create a local server to receive data from
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'application/json' });
-  res.end(JSON.stringify({
-    data: 'Hello World!'
-  }));
+    if(req.url === '/'){
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        fs.readFile('./page/Home.html' , "utf8" , (err,data)=>{
+            if(err) throw err;
+            res.write(data);
+            res.end();
+        })      
+    }
+    else if(req.url === '/about'){
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        fs.readFile('./page/about.html' , "utf8" , (err,data)=>{
+            if(err) throw err;
+            res.write(data);
+            res.end();
+        })       
+    }
+     else if(req.url === '/create-file'){
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        const data = "<h1>This is test file</h1>";
+        fs.appendFile('./temp/test.html' ,data , (err)=>{
+            if(err) throw err;
+            res.write('File is created');
+            res.end();
+        })       
+    }
+    else {
+        res.writeHead(404, { 'Content-Type': 'text/html' });
+        fs.readFile('./page/404.html' , "utf8" , (err,data)=>{
+            if(err) throw err;
+            res.write(data);
+            res.end();
+        })      
+    } 
+    
 });
 
-server.listen(8000);
+console.log(`server is running at http://localhost:${PORT}`);
+server.listen(PORT);
